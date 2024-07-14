860dafd61064613efcbb5ed0b4cff8c8996c4392
ynnaah <janahignacio23@gmail.com>
2024-07-14 14:03:55 +0800

    added backend for view properties guest view and edit properties

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,6 +13,8 @@ import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
 import AddProperty from "./pages/AddProperty";
 import { useAppContext } from "./contexts/AppContext";
+import MyProperties from "./pages/MyProperties";
+import EditProperty from "./pages/EditProperty";
 
 function App() {
 
@@ -24,22 +26,42 @@ function App() {
         <Route path="/" element={<Layout>
           <p>Home Page</p>
         </Layout>} />
-        
-        <Route path="/search" element={<Layout>Search Page</Layout>}/>
-        
-        <Route path="/register" element={<Layout><Register /></Layout>}/>
 
-        <Route path="/sign-in" element={<Layout><SignIn /></Layout>}/>
+        <Route path="/search" element={<Layout>Search Page</Layout>} />
 
-        {isLoggedIn &&
+        <Route path="/register" element={<Layout><Register /></Layout>} />
+
+        <Route path="/sign-in" element={<Layout><SignIn /></Layout>} />
+
+        {isLoggedIn && (
           <>
-            <Route path="/add-property" element={
-              <Layout>
-                <AddProperty />
-              </Layout>
-            }/>
+            <Route
+              path="/add-property"
+              element={
+                <Layout>
+                  <AddProperty />
+                </Layout>
+              }
+            />
+            <Route
+              path="/my-properties"
+              element={
+                <Layout>
+                  <MyProperties />
+                </Layout>
+              }
+            />
+            <Route
+              path="/edit-property/:propertyId"
+              element={
+                <Layout>
+                  <EditProperty />
+                </Layout>
+              }
+            />
           </>
-        }
+        )}
+
 
         {/* Catches all routes */}
         <Route path="*" element={<Navigate to="/" />} />
