0b90cced34eb365291f950c8ba2a4cf867795e68
ynnaah <janahignacio23@gmail.com>
2024-07-16 12:55:44 +0800

    Added navbar and footer (non-responsive)

--- frontend/src/pages/MyProperties.tsx
+++ frontend/src/pages/MyProperties.tsx
@@ -19,6 +19,8 @@ const MyProperties = () => {
 
     return (
         <div className="space-y-5">
+
+            
             <span className="flex justify-between">
                 <h1 className="text-3xl font-bold"> My Property</h1>
                 <Link
@@ -27,11 +29,18 @@ const MyProperties = () => {
                     Add Property
                 </Link>
             </span>
+
+
             <div className="grid grid-cols-1 gap-8">
+                
                 {propertyData.map((property) => (
+
                     <div className="flex flex-col justify-between border border-slate-300 rounded-lg p-8 igap-5">
+
                         <h2 className="text-2xl font-bold">{property.name}</h2>
+
                         <div className="whitespace-pre-line">{property.description}</div>
+
                         <div className="grid grid-cols-5 gap-2">
                             <div className="border border-slate-300 rounded-sm p-3 flex items-center">
                                 <BsMap className="mr-1" />
@@ -53,6 +62,7 @@ const MyProperties = () => {
                                 {property.starRating} Star Rating
                             </div>
                         </div>
+                        
                         <span className="flex justify-end">
                             <Link to={`/edit-property/${property._id}`}
                             className="flex bg-blue-600 text-white text-xl font-bold p-2 hover:bg-blue-500"> View Details </Link>
