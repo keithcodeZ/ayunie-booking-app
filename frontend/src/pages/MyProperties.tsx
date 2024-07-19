9464259cfddbf06ebcf34a4123118a66ae0697d0
ynnaah <janahignacio23@gmail.com>
2024-07-19 13:07:31 +0800

    fixed layout for properties host view

--- frontend/src/pages/MyProperties.tsx
+++ frontend/src/pages/MyProperties.tsx
@@ -1,8 +1,6 @@
 import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import * as apiClient from "../api-client"
-import { BiMoney, BiHotel, BiStar } from "react-icons/bi";
-import { BsMap, BsBuilding } from "react-icons/bs";
 
 const MyProperties = () => {
     const { data: propertyData } = useQuery(
@@ -18,7 +16,7 @@ const MyProperties = () => {
     }
 
     return (
-        <div className="border">
+        <div className="">
 
 
             <span className="flex justify-between py-8 px-8">
@@ -34,44 +32,55 @@ const MyProperties = () => {
 
                 {propertyData.map((property) => (
 
-                    <div className="">
-                        <div className="border">
-
+                    <div className="flex h-64 border">
+                        <div className="w-1/3 border my-4 ml-4">
+                            <img className="object-cover h-56" src={property.imageUrls[0]} />
                         </div>
-                        <div className="border">
 
-                            <h2 className="text-l font-bold">{property.name}</h2>
+                        <div className="w-2/3 px-4">
 
-                            <div className="text-sm">
+                            <h2 className="text-xl font-bold py-2">{property.name}</h2>
 
-                                <div className="rounded-sm flex items-center">
-                                    <BsMap className="mr-1" />
+                            <div className="text-xs">
+
+                                <div className="flex items-center indent-1">
+
+                                    <svg className="h-4 w-4 text-custom-gray" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                                    </svg>
+
                                     {property.city}, {property.country}
                                 </div>
 
-                                <div className="text-sm">{property.description}</div>
+                                <div className="px-4 text-xs text-pretty h-24 m-3 overflow-auto">{property.description}</div>
 
-                                <div className="border border-slate-300 rounded-sm p-3 flex items-center">
-                                    <BsBuilding className="mr-1" />
-                                    {property.type}
-                                </div>
+                                <div className=" rounded-sm flex items-center indent-1">
+                                    <svg className="h-4 w-4 text-custom-gray" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+                                    </svg>
 
-                                <div className="border border-slate-300 rounded-sm p-3 flex items-center">
-                                    <BiMoney className="mr-1" />£{property.pricePerNight} per night
-                                </div>
-
-                                <div className="border border-slate-300 rounded-sm p-3 flex items-center">
-                                    <BiHotel className="mr-1" />
                                     {property.adultCount} adults, {property.childCount} children
                                 </div>
 
-                            </div>
+                                <div className="rounded-sm flex justify-between py-3">
+                                    <div>
+                                    <span className="flex">
+                                        <Link to={`/edit-property/${property._id}`}
+                                            className="text-xs inline-block bg-brown hover:bg-light-brown hover:text-custom-gray text-white py-2 px-5 rounded"> Edit Property Details </Link>
+                                    </span>
+                                    </div>
 
-                            <span className="flex justify-end">
-                                <Link to={`/edit-property/${property._id}`}
-                                    className="flex bg-blue-600 text-white text-xl font-bold p-2 hover:bg-blue-500"> View Details </Link>
-                            </span>
+                                    <div>
+                                    <p className="text-lg text-center text-custom-gray font-bold leading-tight">£{property.pricePerNight}</p>
+                                    <p className="text-right leading-none">includes taxes and fees</p>
+                                    </div>
+
+                                </div>
+
+                            </div>
                         </div>
+
                     </div>
                 ))}
             </div>
