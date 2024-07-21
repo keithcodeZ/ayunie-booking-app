6125b283fc17caa03574f784cd4a118eb37c441c
ynnaah <janahignacio23@gmail.com>
2024-07-21 21:18:39 +0800

    fixed property button

--- frontend/src/components/PropertyMenu.tsx
+++ frontend/src/components/PropertyMenu.tsx
@@ -6,7 +6,7 @@ export default function PropertyMenu() {
       <Menu>
 
         <div>
-          <MenuButton className="inline-flex justify-center hover:bg-light-brown">Properties
+          <MenuButton className="inline-flex justify-center hover:bg-light-brown pl-2">Properties
             <svg xmlns="http://www.w3.org/2000/svg" className="px-1 h-6 w-6 pt-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
               <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
             </svg>
