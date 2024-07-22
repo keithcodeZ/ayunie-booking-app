44bc66a4323c7c3c80169e8dd2b107ccbb1c791d
Karl Matthew Parafina <56289633+karumadesu@users.noreply.github.com>
2024-07-22 20:38:40 +0800

    Updated LatestDestinationCard.tsx

--- frontend/src/components/LatestDestinationCard.tsx
+++ frontend/src/components/LatestDestinationCard.tsx
@@ -1,5 +1,6 @@
 import { AiFillStar } from "react-icons/ai";
 import { PropertyType } from "../../../backend/src/shared/types";
+import { Link } from "react-router-dom";
 
 interface Props {
   property: PropertyType;
@@ -7,27 +8,32 @@ interface Props {
 
 const LatestDestinationCard = ({ property }: Props) => {
   return (
-    <div className="bg-white rounded-lg overflow-hidden shadow-md mb-3">
-      <img
-        src={property.imageUrls[0]}
-        alt={property.name}
-        className="w-full h-40 object-cover"
-      />
-      <div className="p-4">
-        <p className="text-xs text-gray-600">{property.type}</p>
-        <h3 className="text-xl font-bold mb-1">{property.name}</h3>
-        <p className="text-sm mb-2">{property.city}, {property.country}</p>
-        <span className="flex text-sm text-gray-600">
-            {Array.from({ length: property.starRating }).map(() => (
-              <AiFillStar className="fill-yellow-400" />
-            ))}
-        </span>
-        <div className="text-sm text-custom-gray text-center mt-5">
-          <span className="text-xl font-bold">${property.pricePerNight.toFixed(2)}</span>
-          <span>/night</span>
+    <Link
+      to={`/detail/${property._id}`}
+      className="relative cursor-pointer overflow-hidden rounded-md"
+    >
+      <div className="bg-white rounded-lg overflow-hidden shadow-md mb-3">
+        <img
+          src={property.imageUrls[0]}
+          alt={property.name}
+          className="w-full h-40 object-cover"
+        />
+        <div className="p-4">
+          <p className="text-xs text-gray-600">{property.type}</p>
+          <h3 className="text-xl font-bold mb-1">{property.name}</h3>
+          <p className="text-sm mb-2">{property.city}, {property.country}</p>
+          <span className="flex text-sm text-gray-600">
+              {Array.from({ length: property.starRating }).map(() => (
+                <AiFillStar className="fill-yellow-400" />
+              ))}
+          </span>
+          <div className="text-sm text-custom-gray text-center mt-5">
+            <span className="text-xl font-bold">${property.pricePerNight.toFixed(2)}</span>
+            <span>/night</span>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
