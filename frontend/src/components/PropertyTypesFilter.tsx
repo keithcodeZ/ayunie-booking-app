6569c611b98b2d5165b58cd8a93e0f866d5fba4c
Karl Matthew Parafina <56289633+karumadesu@users.noreply.github.com>
2024-07-24 13:16:41 +0800

    Updated filters to be a collapsible component

--- frontend/src/components/PropertyTypesFilter.tsx
+++ frontend/src/components/PropertyTypesFilter.tsx
@@ -1,3 +1,5 @@
+import { useState, useRef, useEffect } from "react";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 import { propertyTypes } from "../config/property-options-config";
 
 type Props = {
@@ -6,21 +8,52 @@ type Props = {
 };
 
 const PropertyTypesFilter = ({ selectedPropertyTypes, onChange }: Props) => {
+  const [isCollapsed, setIsCollapsed] = useState(true);
+  const contentRef = useRef<HTMLDivElement | null>(null);
+
+  const toggleCollapse = () => setIsCollapsed(!isCollapsed);
+
+  useEffect(() => {
+    const content = contentRef.current;
+    if (content) {
+      content.style.transition = "max-height 0.3s ease-in-out";
+      if (isCollapsed) {
+        content.style.maxHeight = "0px";
+      } else {
+        content.style.maxHeight = `${content.scrollHeight}px`;
+      }
+    }
+  }, [isCollapsed]);
+
   return (
     <div className="border-b border-slate-300 pb-5">
-      <h4 className="text-md font-semibold mb-2">Property Type</h4>
-      {propertyTypes.map((propertyType) => (
-        <label className="flex items-center text-sm space-x-2">
-          <input
-            type="checkbox"
-            className="rounded"
-            value={propertyType}
-            checked={selectedPropertyTypes.includes(propertyType)}
-            onChange={onChange}
-          />
-          <span>{propertyType}</span>
-        </label>
-      ))}
+      <div className="flex items-center justify-between mb-2">
+        <h4 className="text-md font-semibold">Property Type</h4>
+        <button onClick={toggleCollapse} className="text-gray-600">
+          {isCollapsed ? <FaChevronDown /> : <FaChevronUp />}
+        </button>
+      </div>
+      <div
+        ref={contentRef}
+        style={{
+          maxHeight: "0px",
+          overflow: "hidden",
+          transition: "max-height 0.3s ease-in-out",
+        }}
+      >
+        {propertyTypes.map((propertyType) => (
+          <label className="flex items-center text-sm space-x-2" key={propertyType}>
+            <input
+              type="checkbox"
+              className="rounded"
+              value={propertyType}
+              checked={selectedPropertyTypes.includes(propertyType)}
+              onChange={onChange}
+            />
+            <span>{propertyType}</span>
+          </label>
+        ))}
+      </div>
     </div>
   );
 };
